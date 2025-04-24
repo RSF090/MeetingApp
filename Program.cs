17114fdedc752fb783542c70d0c4ecd747c9043a
RSF090 <rsfurkan09@gmail.com>
2025-04-24 19:11:42 +0300

    Controllers oluşturuldu

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Builder;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Meeting}/{action=Index}/{id?}"
+);
+
 
 app.Run();
--- obj/Debug/net9.0/MeetingApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MeetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e626a95e8e66d70fa06a8f15c2f0f96c43144a24")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
